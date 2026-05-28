f5906f82baf794576304c9bc173479c9181d8bd9
Guy Scuderi <gjscuderi@gmail.com>
2026-05-28 10:47:24 -0400

    small word edit fix for View Listings on following

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
 // Bump CACHE_VERSION whenever you ship a breaking change to the cached shell so
 // older clients drop the stale cache on activation.
 
-const CACHE_VERSION = 'cc-v66-ephemeral-url-filters';
+const CACHE_VERSION = 'cc-v67-view-listings-link-no-suffix';
 const HTML_NETWORK_TIMEOUT_MS = 2500;
 
 // Cross-origin hostnames whose responses we cache aggressively. Their URLs
