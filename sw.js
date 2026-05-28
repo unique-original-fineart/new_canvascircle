e45ca3cd6b8cacbefcc437cce776f3bdfb83b442
Guy Scuderi <gjscuderi@gmail.com>
2026-05-28 09:58:13 -0400

    Make followed artists have a link to catalog prefiltered for that artist

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
 // Bump CACHE_VERSION whenever you ship a breaking change to the cached shell so
 // older clients drop the stale cache on activation.
 
-const CACHE_VERSION = 'cc-v61-following-collapse-scroll';
+const CACHE_VERSION = 'cc-v62-followed-artist-catalog-link';
 const HTML_NETWORK_TIMEOUT_MS = 2500;
 
 // Cross-origin hostnames whose responses we cache aggressively. Their URLs
