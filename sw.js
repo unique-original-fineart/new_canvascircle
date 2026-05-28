f1bda1e69078fb3aabff4fc05f7544a0ac1ca724
Guy Scuderi <gjscuderi@gmail.com>
2026-05-28 06:12:13 -0400

    implement a share link for listings part A

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
 // Bump CACHE_VERSION whenever you ship a breaking change to the cached shell so
 // older clients drop the stale cache on activation.
 
-const CACHE_VERSION = 'cc-v50-following-handle-tap-fix';
+const CACHE_VERSION = 'cc-v51-share-button';
 const HTML_NETWORK_TIMEOUT_MS = 2500;
 
 // Cross-origin hostnames whose responses we cache aggressively. Their URLs
