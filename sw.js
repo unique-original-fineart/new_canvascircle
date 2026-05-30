6ca71ce9221b639a21f230b767aaa9bc04493c91
Guy Scuderi <gjscuderi@gmail.com>
2026-05-30 07:40:36 -0400

    portal: hide Open-to-Trade looking-for textarea until checkbox is ticked (cc-v119)

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
 // Bump CACHE_VERSION whenever you ship a breaking change to the cached shell so
 // older clients drop the stale cache on activation.
 
-const CACHE_VERSION = 'cc-v118-trade-checkbox-fix';
+const CACHE_VERSION = 'cc-v119-trade-textarea-conditional';
 const HTML_NETWORK_TIMEOUT_MS = 2500;
 
 // Cross-origin hostnames whose responses we cache aggressively. Their URLs
