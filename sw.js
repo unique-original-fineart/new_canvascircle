105ef82ee72110d716b0bcea3881e4c1569562ad
Guy Scuderi <gjscuderi@gmail.com>
2026-05-30 06:52:13 -0400

    Following: show toggle + form on empty state, update help text (cc-v115)

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
 // Bump CACHE_VERSION whenever you ship a breaking change to the cached shell so
 // older clients drop the stale cache on activation.
 
-const CACHE_VERSION = 'cc-v114-ios-audio-unmute-volume-zero';
+const CACHE_VERSION = 'cc-v115-following-toggle-always-visible';
 const HTML_NETWORK_TIMEOUT_MS = 2500;
 
 // Cross-origin hostnames whose responses we cache aggressively. Their URLs
