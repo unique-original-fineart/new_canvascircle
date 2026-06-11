dc0cfd70b4b92904b7f40ea61a515ffe9988a48c
Guy Scuderi <gjscuderi@gmail.com>
2026-06-11 13:52:35 -0400

    cc-v208: deep-stats moved to modal + heat calendar + seasonality + medium donut + trivia card

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
 // Bump CACHE_VERSION whenever you ship a breaking change to the cached shell so
 // older clients drop the stale cache on activation.
 
-const CACHE_VERSION = 'cc-v207-deep-stats-pink-palette';
+const CACHE_VERSION = 'cc-v208-deep-stats-modal-plus-4-new-charts';
 const HTML_NETWORK_TIMEOUT_MS = 2500;
 
 // Cross-origin hostnames whose responses we cache aggressively. As of
