eed6673215a4402ce8b3b1d0e163b82dd17eaa32
Guy Scuderi <gjscuderi@gmail.com>
2026-06-10 20:00:13 -0400

    cc-v193: shareable Collection link + smart default for Collection-only users

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
 // Bump CACHE_VERSION whenever you ship a breaking change to the cached shell so
 // older clients drop the stale cache on activation.
 
-const CACHE_VERSION = 'cc-v192-iso-collection-matching';
+const CACHE_VERSION = 'cc-v193-collection-share-link-and-smart-default';
 const HTML_NETWORK_TIMEOUT_MS = 2500;
 
 // Cross-origin hostnames whose responses we cache aggressively. As of
