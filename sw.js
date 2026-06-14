ca0b0aa7557beb41ce76518d57dba0bd8e061926
Guy Scuderi <gjscuderi@gmail.com>
2026-06-14 19:49:48 -0400

    cc-v214: shift colored-text-on-navy to off-white for legibility

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
 // Bump CACHE_VERSION whenever you ship a breaking change to the cached shell so
 // older clients drop the stale cache on activation.
 
-const CACHE_VERSION = 'cc-v213-heart-pill-contrast';
+const CACHE_VERSION = 'cc-v214-link-and-banner-contrast';
 const HTML_NETWORK_TIMEOUT_MS = 2500;
 
 // Cross-origin hostnames whose responses we cache aggressively. As of
